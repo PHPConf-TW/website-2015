ce5c9746d97fd4de9c945f9b4e0fd3fab46198b1
Bo-Yi Wu <appleboy.tw@gmail.com>
2015-07-12 11:09:23 +0800

    move index.html to src folder.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,12 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
-    './src/index'
+    './src/js/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/assets/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
@@ -24,7 +24,7 @@ module.exports = {
     loaders: [{
       test: /\.jsx?$/,
       loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
+      include: path.join(__dirname, 'src/js')
     }]
   }
 };

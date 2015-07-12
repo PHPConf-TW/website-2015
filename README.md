ea577bd802b941032e324f1c1ff535f28687174c
Bo-Yi Wu <appleboy.tw@gmail.com>
2015-07-12 10:48:23 +0800

    initial react project
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ README.md
@@ -0,0 +1,13 @@
+# PHPConf Website 2015
+
+Souce Code for PHPConf Taiwan 2015.
+
+### Usage
+
+```
+$ npm install
+$ npm start
+```
+
+open http://localhost:3000 in any browser.
+
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <h1>Welcome PHPConf 2015.</h1>
+    );
+  }
+}

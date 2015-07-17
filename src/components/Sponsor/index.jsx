a1bb2f052a2ecad2b4eedbe6ecd94d29b415b11f
Hane Liu <hane0131@gmail.com>
2015-07-17 18:10:04 +0800

    Convert sponsors' content of website-2014 into react component(#11)

--- src/components/Sponsor/index.jsx
+++ src/components/Sponsor/index.jsx
@@ -1,10 +1,47 @@
 import '!style!css!less!./Sponsor.less';
 import React, { Component } from 'react';
+import imfju from './appreciate/imfju.png';
+import tronice from './appreciate/Tronice.png';
+import ossf from './appreciate/ossf.png';
+import microsoft from './diamond/microsoft.jpg';
+import kkbox from './gold/kkbox.png';
+import pumo from './gold/pumo.png';
+import sina from './cooperate/sina.jpg';
+import weibo from './cooperate/weibo.jpg';
+import technews from './cooperate/Technews.logo.png';
 
 class Sponsor extends Component {
+
   render() {
     return (
-      <section />
+      <section id="sponsors">
+        <h3>贊助</h3>
+
+        <div class="sponsor">
+            <h4>鑽石級贊助</h4>
+            <img src={microsoft} />
+        </div>
+        <hr />
+        <div class="sponsor">
+            <h4>黃金級贊助</h4>
+            <img src={kkbox} />
+            <img src={pumo} />
+        </div>
+        <hr />
+        <div class="sponsor">
+            <h4>特別感謝</h4>
+            <img src={ossf} />
+            <img src={imfju} class="imfju" />
+            <img src={tronice} class="tronice" alt="創時系統" />
+        </div>
+        <hr />
+        <div class="sponsor">
+            <h4>合作媒體</h4>
+            <img src={sina} />
+            <img src={weibo} />
+            <img src={technews} />
+        </div>
+    </section>
     );
   };
 };

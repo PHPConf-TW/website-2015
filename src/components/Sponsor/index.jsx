ae4315e3e5aaace2c5e166c952934b25af82ec03
Hane Liu <hane0131@gmail.com>
2015-07-17 20:12:47 +0800

    Declare a variable to store the information of sponsors

--- src/components/Sponsor/index.jsx
+++ src/components/Sponsor/index.jsx
@@ -13,6 +13,65 @@ import technews from './cooperate/Technews.logo.png';
 class Sponsor extends Component {
 
   render() {
+    var sponsorsData = {
+      diamondSponsor:[
+        {
+          name:'Microsoft',
+          classname:'microsoft',
+          logo:'assets/images/sponsor/diamond/microsoft.jpg'
+        }
+      ],
+
+      goldSponsor:[
+        {
+          name:'KKBox',
+          classname:'kkbox',
+          logo:'assets/images/sponsor/gold/kkbox.png'
+        },
+        {
+          name:'捕夢網',
+          classname:'pumo',
+          logo:'assets/images/sponsor/gold/pumo.png'
+        }
+      ],
+
+      speicialAppreciate:[
+        {
+          name:'輔仁大學資訊管理學系',
+          classname:'imfju',
+          logo:'assets/images/sponsor/appreciate/imfju.png'
+        },
+        {
+          name:'OSSF',
+          classname:'ossf',
+          logo:'assets/images/sponsor/appreciate/ossf.png'
+        },
+        {
+          name:'創時系統',
+          classname:'tronice',
+          logo:'assets/images/sponsor/appreciate/Tronice.png'
+        }
+      ],
+
+      cooperantMedia:[
+        {
+          name:'新浪',
+          classname:'sina',
+          logo:'assets/images/sponsor/cooperate/sina.jpg'
+        },
+        {
+          name:'微博',
+          classname:'weibo',
+          logo:'assets/images/sponsor/cooperate/weibo.jpg'
+        },
+        {
+          name:'TechNews',
+          classname:'technews',
+          logo:'assets/images/sponsor/cooperate/Technews.logo.png'
+        }
+      ]
+    };
+
     return (
       <section id="sponsors">
         <h3>贊助</h3>

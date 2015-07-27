729fa2470f97fedad6ffc18d13282ccff23de658
Bo-Yi Wu <appleboy.tw@gmail.com>
2015-07-28 00:01:39 +0800

    rewrite render.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- src/components/Speaker/index.jsx
+++ src/components/Speaker/index.jsx
@@ -3,57 +3,62 @@ import React, { Component } from 'react';
 
 class Speaker extends Component {
   render() {
-    var speakers = [
+
+    let speakers = [
       {
-        avatar:'http://fakeimg.pl/120x120',
-        name:'Brian',
-        title:'SBACE.co 軟體開發總監',
-        info:[
+        avatar: 'http://fakeimg.pl/120x120',
+        name: 'Brian',
+        title: 'SBACE.co 軟體開發總監',
+        info: [
           'iOS/PHP Developer，目前為 SBACE.co 軟體開發總監。',
           '過去於 CyberLink 擔綱資深工程師，也曾是 5945.tw 技術長暨共同創辦人。',
           '寫過兩本 iOS 開發教學書籍，30歲以前熱愛參加軟體開發競賽和 Hack Day，得過兩次小獎，喜歡 Cafe，鋼琴樂和巧克力飲。'
         ]
       },
       {
-        avatar:'http://fakeimg.pl/120x120',
-        name:'Joe',
-        title:'ProjectUp',
-        info:[
+        avatar: 'http://fakeimg.pl/120x120',
+        name: 'Joe',
+        title: 'ProjectUp',
+        info: [
           'Joe Chang, PMP，識博管理顧問執行長，也是華人最大的專案部落格 -「專案管理生活思維」的站長。',
           'Joe具備多年企業診斷、專案輔導、流程設計、及EPPM系統導入經驗。從專案成員、PM、PMO主管、CEO都擔任過，非常了解專案各階層角色的困境與心聲。 是台灣少數同時具備Oracle Primavera Portfolio Management Implementation及微軟Project Portfolio Management認證的資深顧問。近七年來連續不間斷的透過部落格，試圖讓專案管理的理念深化到每個人身上。'
         ]
       },
     ];
-    var ASpeaker = React.createClass({
-    render:function(){
-      var info = this.props.speaker.info.map(function(content) {
-        return <p>{content}</p>;
-      });
-      return (
-        <div className="speaker">
-          <img className="avatar" src={this.props.speaker.avatar} />
-          <div className="speaker-info">
-            <h4>{this.props.speaker.name}</h4>
-            <h5>{this.props.speaker.title}</h5>
-            {info}
+
+    let ASpeaker = React.createClass({
+      render() {
+        let info = this.props.speaker.info.map((content) => {
+          return <p>{content}</p>
+        });
+
+        return (
+          <div className="speaker">
+            <img className="avatar" src={this.props.speaker.avatar} />
+            <div className="speaker-info">
+              <h4>{this.props.speaker.name}</h4>
+              <h5>{this.props.speaker.title}</h5>
+              {info}
+            </div>
+            <hr />
           </div>
-          <hr />
-        </div>
-      );
-    }
-  });
-  var Speakers = React.createClass({
-    render:function(){
-      var speakers=this.props.speakers.map(function(speaker){
-        return <ASpeaker speaker={speaker} />;
-      });
-      return (
-        <div>
-          {speakers}
-        </div>
-      );
-    }
-  });
+        )
+      }
+    });
+
+    let Speakers = React.createClass({
+      render() {
+        let speakers=this.props.speakers.map((speaker) => {
+          return <ASpeaker speaker={speaker} />;
+        });
+
+        return (
+          <div>
+            {speakers}
+          </div>
+        )
+      }
+    });
     return (
       <section id="speakers">
         <h3>講師</h3>
--- src/components/Sponsor/index.jsx
+++ src/components/Sponsor/index.jsx
@@ -4,75 +4,77 @@ import React, { Component } from 'react';
 class Sponsor extends Component {
 
   render() {
-    var sponsorsData = {
+    let sponsorsData = {
       diamondSponsor:[
         {
-          name:'Microsoft',
-          classname:'microsoft',
-          logo:'assets/images/sponsor/diamond/microsoft.jpg'
+          name: 'Microsoft',
+          classname: 'microsoft',
+          logo: 'assets/images/sponsor/diamond/microsoft.jpg'
         }
       ],
 
       goldSponsor:[
         {
-          name:'KKBox',
-          classname:'kkbox',
-          logo:'assets/images/sponsor/gold/kkbox.png'
+          name: 'KKBox',
+          classname: 'kkbox',
+          logo: 'assets/images/sponsor/gold/kkbox.png'
         },
         {
-          name:'捕夢網',
-          classname:'pumo',
-          logo:'assets/images/sponsor/gold/pumo.png'
+          name: '捕夢網',
+          classname: 'pumo',
+          logo: 'assets/images/sponsor/gold/pumo.png'
         }
       ],
 
       speicialAppreciate:[
         {
-          name:'輔仁大學資訊管理學系',
-          classname:'imfju',
-          logo:'assets/images/sponsor/appreciate/imfju.png'
+          name: '輔仁大學資訊管理學系',
+          classname: 'imfju',
+          logo: 'assets/images/sponsor/appreciate/imfju.png'
         },
         {
-          name:'OSSF',
-          classname:'ossf',
-          logo:'assets/images/sponsor/appreciate/ossf.png'
+          name: 'OSSF',
+          classname: 'ossf',
+          logo: 'assets/images/sponsor/appreciate/ossf.png'
         },
         {
-          name:'創時系統',
-          classname:'tronice',
-          logo:'assets/images/sponsor/appreciate/Tronice.png'
+          name: '創時系統',
+          classname: 'tronice',
+          logo: 'assets/images/sponsor/appreciate/Tronice.png'
         }
       ],
 
       cooperantMedia:[
         {
-          name:'新浪',
-          classname:'sina',
-          logo:'assets/images/sponsor/cooperate/sina.jpg'
+          name: '新浪',
+          classname: 'sina',
+          logo: 'assets/images/sponsor/cooperate/sina.jpg'
         },
         {
-          name:'微博',
-          classname:'weibo',
-          logo:'assets/images/sponsor/cooperate/weibo.jpg'
+          name: '微博',
+          classname: 'weibo',
+          logo: 'assets/images/sponsor/cooperate/weibo.jpg'
         },
         {
-          name:'TechNews',
-          classname:'technews',
-          logo:'assets/images/sponsor/cooperate/Technews.logo.png'
+          name: 'TechNews',
+          classname: 'technews',
+          logo: 'assets/images/sponsor/cooperate/Technews.logo.png'
         }
       ]
     };
 
-    var SponsorList=React.createClass({
-      render: function(){
-        var sponsors = this.props.data.map(function(sponsor){
-          return <img src={sponsor.logo} className={sponsor.classname} alt={sponsor.name}/>
+    let SponsorList = React.createClass({
+      render() {
+
+        let sponsors = this.props.data.map((sponsor) => {
+          return <img src={sponsor.logo} className={sponsor.classname} alt={sponsor.name} />
         });
+
         return (
-            <div className="sponsor">
-              <h4>{this.props.type}</h4>
-              {sponsors}
-            </div>
+          <div className="sponsor">
+            <h4>{this.props.type}</h4>
+            {sponsors}
+          </div>
         );
       }
     });

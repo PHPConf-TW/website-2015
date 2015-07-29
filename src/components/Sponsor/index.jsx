6421f2d9ce7329224021181ba9bec49371d387aa
Bo-Yi Wu <appleboy.tw@gmail.com>
2015-07-29 20:03:57 +0800

    #17 add sponsor yml.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- src/components/Sponsor/index.jsx
+++ src/components/Sponsor/index.jsx
@@ -1,7 +1,7 @@
 import '!style!css!less!./Sponsor.less';
 import React, { Component } from 'react';
 import SponsorList from './List';
-import data from './data';
+import SponsorData from 'json!yaml!../../../data/sponsors.yml';
 
 class Sponsor extends Component {
 
@@ -9,13 +9,13 @@ class Sponsor extends Component {
     return (
       <section id="sponsors">
         <h3>贊助</h3>
-        <SponsorList type="鑽石級贊助" data={data.diamondSponsor} />
+        <SponsorList type="鑽石級贊助" data={SponsorData.diamondSponsor} />
         <hr />
-        <SponsorList type="黃金級贊助" data={data.goldSponsor} />
+        <SponsorList type="黃金級贊助" data={SponsorData.goldSponsor} />
         <hr />
-        <SponsorList type="特別感謝" data={data.speicialAppreciate} />
+        <SponsorList type="特別感謝" data={SponsorData.speicialAppreciate} />
         <hr />
-        <SponsorList type="合作媒體" data={data.cooperantMedia} />
+        <SponsorList type="合作媒體" data={SponsorData.cooperantMedia} />
       </section>
     );
   };

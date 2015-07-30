0b07ddffd82527a547762f4a44bd74646129038d
bificoo <bificoo@gmail.com>
2015-07-30 15:05:48 +0800

    #5 update speaker photo mask

--- src/components/Speaker/index.jsx
+++ src/components/Speaker/index.jsx
@@ -13,7 +13,9 @@ class Speaker extends Component {
 
         return (
           <div className="pure-u-1 pure-u-md-1-3 pure-u-lg-1-4 speaker">
-            <img className="avatar" src={this.props.speaker.avatar} />
+            <div className="avatar">
+              <img src={this.props.speaker.avatar} />
+            </div>
             <div className="speaker-info">
               <h5>{this.props.speaker.title}</h5>
               <h4>{this.props.speaker.name}</h4>

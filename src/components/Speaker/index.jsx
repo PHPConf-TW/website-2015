4cb05ef5c9b1ec50198815cc3af08ca09d242dfd
bificoo <bificoo@gmail.com>
2015-07-30 14:52:57 +0800

    #5 update speaker style

--- src/components/Speaker/index.jsx
+++ src/components/Speaker/index.jsx
@@ -12,7 +12,7 @@ class Speaker extends Component {
         });
 
         return (
-          <div className="pure-u-1 pure-u-md-1-3 speaker">
+          <div className="pure-u-1 pure-u-md-1-3 pure-u-lg-1-4 speaker">
             <img className="avatar" src={this.props.speaker.avatar} />
             <div className="speaker-info">
               <h5>{this.props.speaker.title}</h5>

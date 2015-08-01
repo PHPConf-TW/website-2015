f1e4475882c3928e5e80832cbc84adea72f5121d
bificoo <bificoo@gmail.com>
2015-08-01 16:20:50 +0800

    # update schedule style

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -6,17 +6,17 @@ class SignUp extends Component {
     return (
       <section id="signup">
         <div className="row">
-            <h3>報名</h3>
-            <p className="text-center">PHPConf 2015 報名日期有兩個時段，注意時間趕快來報名唷 ^.^ ~ (我是假文案)</p>
-            <div className="pure-u-1 pure-u-md-1-2 f-date">
-                <div>2014年9月18日 20:00</div>
-            </div>
-            <div className="pure-u-1 pure-u-md-1-2 l-date">
-                <div>2014年9月24日 20:00</div>
-            </div>
-            <div className="pure-u-1 text-center signup-btn">
-                <a href="" className="pure-button btn" target="_blank">前往報名</a>
-            </div>
+          <h3>報名</h3>
+          <p className="text-center">PHPConf 2015 報名日期有兩個時段，注意時間趕快來報名唷 ^.^ ~ (我是假文案)</p>
+          <div className="pure-u-1 pure-u-md-1-2 f-date">
+            <div>2014年9月18日 20:00</div>
+          </div>
+          <div className="pure-u-1 pure-u-md-1-2 l-date">
+            <div>2014年9月24日 20:00</div>
+          </div>
+          <div className="pure-u-1 text-center signup-btn">
+            <a href="" className="pure-button btn" target="_blank">前往報名</a>
+          </div>
         </div>
       </section>
     );

fa8adca62a3d7b79b102ba5ff58ea289ee5e61ee
bificoo <bificoo@gmail.com>
2015-08-04 18:58:11 +0800

    #5 update nav menu for mobile

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -2,20 +2,44 @@ import '!style!css!less!./Nav.less';
 import React, { Component } from 'react';
 
 class Nav extends Component {
+
+  componentDidMount() {
+    $(function() {
+      let $nav = $('nav'), $hamburger = $nav.find('.hamburger'), $menu = $nav.find('ul');
+      $hamburger.on('click', function() {
+        if ($nav.hasClass('active')) {
+          $nav.removeClass('active');
+        } else {
+          $nav.addClass('active');
+        }
+      });
+      $menu.on('click', function() {
+        if ($nav.hasClass('active')) {
+          $nav.removeClass('active');
+        }
+      });
+    });
+  }
+
   render() {
     return (
       <nav>
         <div className="row">
+          <div className="hamburger">
+            <div className="layer top"></div>
+            <div className="layer mid"></div>
+            <div className="layer bottom"></div>
+          </div>
           <div className="logo">PHPConf 2015</div>
           <ul>
-            <li><a href='#top-section'>首頁</a></li>
-            <li><a href='#signup-section'>報名</a></li>
-            <li><a href='#prospect-section'>展望</a></li>
-            <li><a href='#speakers-section'>講師</a></li>
-            <li><a href='#schedule-section'>議程</a></li>
-            <li><a href='#venue-section'>地點</a></li>
-            <li><a href='#about-section'>關於</a></li>
-            <li><a href='#sponsors-section'>贊助</a></li>
+            <li><a href='#top-section'><div>首頁</div></a></li>
+            <li><a href='#signup-section'><div>報名</div></a></li>
+            <li><a href='#prospect-section'><div>展望</div></a></li>
+            <li><a href='#speakers-section'><div>講師</div></a></li>
+            <li><a href='#schedule-section'><div>議程</div></a></li>
+            <li><a href='#venue-section'><div>地點</div></a></li>
+            <li><a href='#about-section'><div>關於</div></a></li>
+            <li><a href='#sponsors-section'><div>贊助</div></a></li>
           </ul>
         </div>
       </nav>

3f94114c42d1623b69a9ad33c77ca41d4552f5cc
KeaNy Chu <keanyc@gmail.com>
2015-08-04 23:45:59 +0800

    Update banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -7,7 +7,7 @@ class Banner extends Component {
   render() {
     return (
       <section id="banner" style={{backgroundImage: `url('${BannerImage}')`}}>
-        <img alt='PHPConf Taiwan logo' title='PHPConf Taiwan' src={Logo} />
+        <img alt='PHPConf Taiwan logo' title='PHPConf Taiwan 2015' src={Logo} />
         <h1>PHPConf Taiwan 2015</h1>
         <div className="mask"></div>
       </section>

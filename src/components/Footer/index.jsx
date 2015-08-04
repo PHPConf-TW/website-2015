ead00117913f0cadd8663fa340dc3d71151472d6
KeaNy Chu <keanyc@gmail.com>
2015-08-04 23:36:39 +0800

    Update footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,12 +6,9 @@ class Footer extends Component {
     return (
       <footer>
         <ul>
-          <li><a href="http://phpconf.tw/2014">2014</a></li>
-          <li><a href="http://phpconf.tw/2013">2013</a></li>
-          <li><a href="http://phpconf.tw/2012">2012</a></li>
-          <li><a href="http://phpconf.tw/2011">2011</a></li>
+          <li><a href="https://www.facebook.com/groups/551533458209944/" target="_blank">Facebook 社團</a></li>
+          <li><a href="mailto:contact@phpconf.tw">聯絡我們</a></li>
         </ul>
-        <a href="mailto:contact@phpconf.tw">contact@phpconf.tw</a>
       </footer>
     );
   }

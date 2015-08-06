3095e92c53e0af8f8ed39b8b5a1079a4a8777e17
KeaNy Chu <keanyc@gmail.com>
2015-08-06 21:21:05 +0800

    Fix cursor and click event

--- src/components/Speaker/Speaker.jsx
+++ src/components/Speaker/Speaker.jsx
@@ -13,16 +13,16 @@ const Speaker = React.createClass({
           <div onClick={this.handleClick} className="avatar">
             <img src={this.props.avatar} />
           </div>
-          <div onClick={this.handleClick} className="speaker-info">
-            <h5>{this.props.title}</h5>
-            <h4>{this.props.name}</h4>
+          <div className="speaker-info">
+            <h5 onClick={this.handleClick}>{this.props.title}</h5>
+            <h4 onClick={this.handleClick}>{this.props.name}</h4>
             <div className="links">
               <ul>
                 <li><a href="#" target="_blank">FB</a></li>
                 <li><a href="#" target="_blank">相關連結</a></li>
               </ul>
             </div>
-            <p>{_.first(this.props.info)}</p>
+            <p onClick={this.handleClick}>{_.first(this.props.info)}</p>
           </div>
           <SpeakerInfo {...this.props} show={this.state.toggle} handleClick={this.handleClick} />
         </div>

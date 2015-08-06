271b1dac9ac95397b2450f7a35eee65a87972ac7
Bo-Yi Wu <appleboy.tw@gmail.com>
2015-08-06 13:52:46 +0800

    fix #32 add previous wbe site.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -9,6 +9,12 @@ class Footer extends Component {
           <li><a href="https://www.facebook.com/groups/551533458209944/" target="_blank">Facebook 社團</a></li>
           <li><a href="mailto:contact@phpconf.tw">聯絡我們</a></li>
         </ul>
+        <ul>
+          <li><a href="http://phpconf.tw/2014">2014</a></li>
+          <li><a href="http://phpconf.tw/2013">2013</a></li>
+          <li><a href="http://phpconf.tw/2012">2012</a></li>
+          <li><a href="http://phpconf.tw/2011">2011</a></li>
+        </ul>
       </footer>
     );
   }

fdd283f7b65cc3907bb94adcca91a3cb7351a5af
Bo-Yi Wu <appleboy.tw@gmail.com>
2015-08-15 08:51:15 +0800

    fix #44 add more function.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- src/components/Schedule/Content.jsx
+++ src/components/Schedule/Content.jsx
@@ -1,7 +1,9 @@
 import '!style!css!less!./Schedule.less';
 import React, { Component } from 'react';
+import Detail from './Detail';
 
 class Content extends Component {
+
   render() {
     let first;
     let second;
@@ -12,26 +14,8 @@ class Content extends Component {
       <tr className="main-content">
         <td className="time right"></td>
         <td className="content">
-          <div className="pure-u-1 pure-u-md-1-2 one close">
-            {first.content.map((content, key) => {
-              return <p key={key} className="hide">{content}</p>;
-            })}
-            <a className="text-btn">看更多 <i className="fa fa-caret-right"></i></a>
-            <p style={{display: (first.slide_url || first.youtube_url) ? 'inline' : 'none'}} className="slide">
-              <a style={{display: first.slide_url ? 'inline' : 'none'}} href={first.slide_url}>投影片</a>
-              <a style={{display: first.youtube_url ? 'inline' : 'none'}} href={first.youtube_url} target="_blank">Youtube</a>
-            </p>
-          </div>
-          <div className="pure-u-1 pure-u-md-1-2 two close">
-            {second.content.map((content, key) => {
-              return <p key={key} className="hide">{content}</p>;
-            })}
-            <a className="text-btn">看更多 <i className="fa fa-caret-right"></i></a>
-            <p style={{display: (second.slide_url || second.youtube_url) ? 'inline' : 'none'}} className="slide">
-              <a style={{display: second.slide_url ? 'inline' : 'none'}} href={second.slide_url}>投影片</a>
-              <a style={{display: second.youtube_url ? 'inline' : 'none'}} href={second.youtube_url} target="_blank">Youtube</a>
-            </p>
-          </div>
+          <Detail data={first} css="one" />
+          <Detail data={second} css="two" />
         </td>
       </tr>
     );
@@ -40,7 +24,6 @@ class Content extends Component {
 
 Content.propTypes = {
   data: React.PropTypes.array.isRequired,
-  time: React.PropTypes.string.isRequired,
 };
 
 export default Content;
---
+++ src/components/Schedule/Detail.jsx
@@ -0,0 +1,44 @@
+import '!style!css!less!./Schedule.less';
+import React, { Component } from 'react';
+import classnames from 'classnames';
+
+class Detail extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      hide: true,
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      hide: !this.state.hide,
+    });
+  }
+
+  render() {
+    const hideContent = classnames({hide: this.state.hide});
+    const hideButton = classnames('text-btn', {hide: !this.state.hide});
+
+    return (
+      <div className={`pure-u-1 pure-u-md-1-2 ${this.props.css} close`}>
+        {this.props.data.content.map((content, key) => {
+          return <p key={key} className={hideContent}>{content}</p>;
+        })}
+        <a onClick={this.handleClick} className={hideButton}>看更多 <i className="fa fa-caret-right"></i></a>
+        <p style={{display: (this.props.data.slide_url || this.props.data.youtube_url) ? 'inline' : 'none'}} className="slide">
+          <a style={{display: this.props.data.slide_url ? 'inline' : 'none'}} href={this.props.data.slide_url}>投影片</a>
+          <a style={{display: this.props.data.youtube_url ? 'inline' : 'none'}} href={this.props.data.youtube_url} target="_blank">Youtube</a>
+        </p>
+      </div>
+    );
+  }
+}
+
+Detail.propTypes = {
+  data: React.PropTypes.array.isRequired,
+  css: React.PropTypes.string.isRequired,
+};
+
+export default Detail;

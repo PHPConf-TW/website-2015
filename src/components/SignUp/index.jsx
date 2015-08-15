d0430c8e45d1aa05df4b0f3908329ae5ef3dbb32
bificoo <bificoo@gmail.com>
2015-08-15 22:06:07 +0800

    #5 update signup style

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -9,8 +9,8 @@ class SignUp extends Component {
           <h3>報名</h3>
           <div className="text-center">
 
-            <div className="pure-u-1 pure-u-md-1-2 title">台灣年度 PHP 聚會於10月9日在輔仁大學國璽樓舉行。</div>
-            <div className="pure-u-1 pure-u-md-1-2 date">開放報名時間 <span className="time">2015/09/14</span> 中午 12:00 (Taipei/Asia)</div>
+            <div className="pure-u-1 pure-u-md-1-2 title">台灣年度 PHP 聚會於 <span className="time">2015/10/9</span> 舉行</div>
+            <div className="pure-u-1 pure-u-md-1-2 date">報名時間 <span className="time">2015/09/14</span> 中午 12:00 (Taipei/Asia)</div>
           </div>
           <div className="pure-u-1 text-center signup-btn">
             <a href="" className="pure-button btn" target="_blank">立即報名</a>

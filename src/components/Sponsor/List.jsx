a70c16809dc1551c4e78491e0f3291c53b1e3076
KeaNy Chu <keanyc@gmail.com>
2015-09-06 23:47:16 +0800

    Update sponsor

--- src/components/Sponsor/List.jsx
+++ src/components/Sponsor/List.jsx
@@ -4,7 +4,21 @@ class List extends Component {
 
   render() {
     const sponsors = this.props.data.map((sponsor, index) => {
-      return <div key={index} className="sponsor pure-u-1-2 pure-u-md-1-3"><a href={sponsor.link} target="_blank"><div className="sponsor-in"><div className={sponsor.categoryclass}><i className={sponsor.iconclass}></i> {sponsor.categoryname}</div><img src={sponsor.logo} className={sponsor.classname} title={sponsor.name} /></div></a></div>;
+      return (
+        <div key={index} className="sponsor pure-u-1-2 pure-u-md-1-3">
+          <a href={sponsor.link} target="_blank">
+            <div className="sponsor-in">
+              <div className={sponsor.categoryclass}>
+                <i className={sponsor.iconclass}></i> {sponsor.categoryname}
+              </div>
+              <img src={sponsor.logo} className={sponsor.classname} title={sponsor.name} />
+              <div className="intro" title={sponsor.intro}>
+                {sponsor.intro}
+              </div>
+            </div>
+          </a>
+        </div>
+      );
     });
 
     return (
